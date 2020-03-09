f6e88cd9f8bf5720c34b3e5cf20af09212b592d0
JoyceMassau <joyce@coesma.com.br>
2020-03-09 11:42:11 -0300

    Demonstra propriedade  privada de uma classe dentro da classe

--- alura/bin/main.dart
+++ alura/bin/main.dart
@@ -5,5 +5,8 @@ void main() {
 
   ContaCorrente contaDaAmanda = ContaCorrente();
   ContaCorrente contaDoTiago = ContaCorrente();
+
+  contaDaAmanda.saldo = -101;
+  print(contaDaAmanda.saldo);
 }
 
