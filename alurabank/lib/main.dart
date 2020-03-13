5b222d4739448b3566ab09fdbad016c1f25bf88d
JoyceMassau <joyce@coesma.com.br>
2020-03-13 11:59:53 -0300

    Cria Editor para reutilizar código repetitivo FormularioTransferencias

--- alurabank/lib/main.dart
+++ alurabank/lib/main.dart
@@ -24,20 +24,7 @@ class FormularioTransferencias extends StatelessWidget {
       appBar: AppBar(title: Text('Criando Transferências')), 
       body: Column(
         children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: TextField(
-            controller: _controladorCampoNumeroConta,
-              style: TextStyle(
-                fontSize: 24.0,
-              ),
-              decoration: InputDecoration(
-                labelText: 'Número da Conta',
-                hintText: '0000',
-              ),
-              keyboardType: TextInputType.number,
-            ),
-          ),
+          
           Padding(
             padding: const EdgeInsets.all(16.0),
             child: TextField(
@@ -67,6 +54,33 @@ class FormularioTransferencias extends StatelessWidget {
           ),
         ],
       )
+    );
+  }
+}
+
+class Editor extends StatelessWidget {
+
+  final TextEditingController _controlador;
+  final String _rotulo;
+  final String _dica;
+
+  Editor(this._controlador, this._rotulo, this._dica);
+  
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: TextField(
+      controller: _controlador,
+        style: TextStyle(
+          fontSize: 24.0,
+        ),
+        decoration: InputDecoration(
+          labelText: _rotulo,
+          hintText: _dica,
+        ),
+        keyboardType: TextInputType.number,
+      ),
     );
   }
 }
