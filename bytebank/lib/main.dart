a1dc887bdf0ee3ae0c1aca1155bf0d57be216f93
JoyceMassau <joyce@coesma.com.br>
2020-03-17 14:46:11 -0300

    Descomenta assets de imagens arquivo pubspec.yaml

--- bytebank/lib/main.dart
+++ bytebank/lib/main.dart
@@ -13,7 +13,7 @@ class BytebankApp extends StatelessWidget {
         ),
         body: Column(
           children: <Widget>[
-            Image.network('https://cdn.pixabay.com/photo/2016/03/05/19/02/hamburger-1238246_960_720.jpg')
+            Image.asset(name)
           ],
         ),
       ),
