3bfca6a04c37e8cb1bd0b3950f95bd4887cf5cb1
JoyceMassau <joyce@coesma.com.br>
2020-03-17 16:54:47 -0300

    Estiliza visual com InkWell e Material

--- bytebank/lib/screens/dashboard.dart
+++ bytebank/lib/screens/dashboard.dart
@@ -18,35 +18,38 @@ class Dashboard extends StatelessWidget {
           ),
           Padding(
             padding: const EdgeInsets.all(8.0),
-            child: InkWell(
-              onTap: () {
-                Navigator.of(context).push(MaterialPageRoute(
-                  builder: (context) => ContactsList(),
-                ));
-              },
-              child: Container(
-                padding: EdgeInsets.all(8.0),
-                height: 100,
-                width: 150,
-                color: Theme.of(context).primaryColor,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Icon(
-                      Icons.people, 
-                      color: Colors.white,
-                      size: 24.0,
-                    ),
-                    Text(
-                      'Contatos',
-                      style: TextStyle(
+            child: Material(
+              color: Theme.of(context).primaryColor,
+              child: InkWell(
+                onTap: () {
+                  Navigator.of(context).push(MaterialPageRoute(
+                    builder: (context) => ContactsList(),
+                  ));
+                },
+                child: Container(
+                  padding: EdgeInsets.all(8.0),
+                  height: 100,
+                  width: 150,
+                  color: Theme.of(context).primaryColor,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Icon(
+                        Icons.people, 
                         color: Colors.white,
-                        fontSize: 16.0, 
+                        size: 24.0,
                       ),
-                    )
-                  ],
-                )
+                      Text(
+                        'Contatos',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 16.0, 
+                        ),
+                      )
+                    ],
+                  )
+                ),
               ),
             ),
           )
