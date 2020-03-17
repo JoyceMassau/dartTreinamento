e68d4f6895ea570b6050a11537155c8f1bdb2635
JoyceMassau <joyce@coesma.com.br>
2020-03-17 14:42:43 -0300

    Empilha widgets em Colunas e define imagem URL

--- bytebank/lib/main.dart
+++ bytebank/lib/main.dart
@@ -8,7 +8,14 @@ class BytebankApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: Text('Dashboard'),)
+        appBar: AppBar(
+          title: Text('Dashboard'),
+        ),
+        body: Column(
+          children: <Widget>[
+            Image.network('https://cdn.pixabay.com/photo/2016/03/05/19/02/hamburger-1238246_960_720.jpg')
+          ],
+        ),
       ),
     );
   }
