3608c83d802a0baa54b218c0cfa72afb742fc62c
JoyceMassau <joyce@coesma.com.br>
2020-03-17 14:38:22 -0300

    Acrescenta widgets à tela inicial

--- bytebank/lib/main.dart
+++ bytebank/lib/main.dart
@@ -6,6 +6,10 @@ class BytebankApp extends StatelessWidget {
   
   @override
   Widget build(BuildContext context) {
-
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text('Dashboard'),)
+      ),
+    );
   }
 }
