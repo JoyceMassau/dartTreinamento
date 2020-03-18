06098a4c4c7e2a00efa14412492323be8160cb77
JoyceMassau <joyce@coesma.com.br>
2020-03-18 15:01:25 -0300

    Abre caminho banco de dados com openDatabase

--- bytebank/lib/database/app_database.dart
+++ bytebank/lib/database/app_database.dart
@@ -4,5 +4,6 @@ import 'package:sqflite/sqflite.dart';
 void createDatabase() {
   getDatabasesPath().then((dbPath) {
     final String path = join(dbPath, 'bytebank.db');
+    openDatabase(path);
   });
 }
