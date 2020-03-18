6282718da63348ddb923ae42b9dd1afc2701cb98
JoyceMassau <joyce@coesma.com.br>
2020-03-18 14:59:49 -0300

    Função Join da dependência para juntar strings

--- bytebank/lib/database/app_database.dart
+++ bytebank/lib/database/app_database.dart
@@ -1,7 +1,8 @@
+import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
 
 void createDatabase() {
   getDatabasesPath().then((dbPath) {
-    
+    join(dbPath, 'bytebank.db');
   });
 }
