1f90cf0d8fcd1e8a3d6114859535f787207ddd72
Massau <joyce.santos202@etec.sp.gov.br>
2020-03-18 01:21:43 -0300

    Cria estrutura input  tela contact form

--- bytebank/lib/screens/contact_form.dart
+++ bytebank/lib/screens/contact_form.dart
@@ -4,7 +4,21 @@ class ContactForm extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('New Contact')),
+      appBar: AppBar(
+        title: Text('New Contact'),
+      ),
+      body: Column(
+        children: <Widget>[
+          TextField(
+            decoration: InputDecoration(
+              labelText: 'Full Name',
+            ),
+            style: TextStyle(
+              fontSize: 24.0,
+            ),
+          ),
+        ]
+      ),
     );
   }
 }
