6a1d11f42b59c4b8f3dba7d1f63d2cca024f3372
JoyceMassau <joyce@coesma.com.br>
2020-03-19 16:10:53 -0300

    Converte em string AccountNumber

--- bytebank/lib/screens/contacts_list.dart
+++ bytebank/lib/screens/contacts_list.dart
@@ -33,7 +33,7 @@ class ContactsList extends StatelessWidget {
 }
 
 class _ContactItem extends StatelessWidget {
-  
+
   final Contact contact;
 
   _ContactItem(this.contact);
@@ -43,7 +43,7 @@ class _ContactItem extends StatelessWidget {
     return Card(
       child: ListTile(
         title: Text(contact.name, style: TextStyle(fontSize: 24.0),),
-        subtitle: Text(contact.accountNumber, style: TextStyle(fontSize: 16.0),),
+        subtitle: Text(contact.accountNumber.toString(), style: TextStyle(fontSize: 16.0),),
       )
     );
   }
