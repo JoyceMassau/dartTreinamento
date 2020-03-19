65eb187b2d752d25e1fc84aee4d9954286a03c69
JoyceMassau <joyce@coesma.com.br>
2020-03-19 16:09:42 -0300

    Altera informações fixas da classe _ContactItem

--- bytebank/lib/screens/contacts_list.dart
+++ bytebank/lib/screens/contacts_list.dart
@@ -1,3 +1,4 @@
+import 'package:bytebank/models/contact.dart';
 import 'package:bytebank/screens/contact_form.dart';
 import 'package:flutter/material.dart';
 
@@ -32,12 +33,17 @@ class ContactsList extends StatelessWidget {
 }
 
 class _ContactItem extends StatelessWidget {
+  
+  final Contact contact;
+
+  _ContactItem(this.contact);
+
   @override
   Widget build(BuildContext context) {
     return Card(
       child: ListTile(
-        title: Text('Alex', style: TextStyle(fontSize: 24.0),),
-        subtitle: Text('1000', style: TextStyle(fontSize: 16.0),),
+        title: Text(contact.name, style: TextStyle(fontSize: 24.0),),
+        subtitle: Text(contact.accountNumber, style: TextStyle(fontSize: 16.0),),
       )
     );
   }
