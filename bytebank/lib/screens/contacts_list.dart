6e0c6d0a22bad88ff78be7f11f67e2dc3a41ebac
JoyceMassau <joyce@coesma.com.br>
2020-03-20 09:38:23 -0300

    Importa database para utilizar findAll

--- bytebank/lib/screens/contacts_list.dart
+++ bytebank/lib/screens/contacts_list.dart
@@ -1,3 +1,4 @@
+import 'package:bytebank/database/app_database.dart';
 import 'package:bytebank/models/contact.dart';
 import 'package:bytebank/screens/contact_form.dart';
 import 'package:flutter/material.dart';
