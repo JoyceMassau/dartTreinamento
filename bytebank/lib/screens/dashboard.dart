84d307bcbbd3ccdac5eb6219f157aed630f2c83a
JoyceMassau <joyce@coesma.com.br>
2020-04-14 10:44:16 -0300

    Duplica padding e os cria dentro de uma coluna

--- bytebank/lib/screens/dashboard.dart
+++ bytebank/lib/screens/dashboard.dart
@@ -52,6 +52,43 @@ class Dashboard extends StatelessWidget {
                 ),
               ),
             ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Material(
+              color: Theme.of(context).primaryColor,
+              child: InkWell(
+                onTap: () {
+                  Navigator.of(context).push(MaterialPageRoute(
+                    builder: (context) => ContactsList(),
+                  ));
+                },
+                child: Container(
+                  padding: EdgeInsets.all(8.0),
+                  height: 100,
+                  width: 150,
+                  color: Theme.of(context).primaryColor,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Icon(
+                        Icons.monetization_on, 
+                        color: Colors.white,
+                        size: 24.0,
+                      ),
+                      Text(
+                        'Transfer',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 16.0, 
+                        ),
+                      )
+                    ],
+                  )
+                ),
+              ),
+            ),
           )
         ],
       ),
