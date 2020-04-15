5f4b9457be0bc24d4414c263c688a061b5854538
JoyceMassau <joyce@coesma.com.br>
2020-04-15 19:05:54 -0300

    Converte json e cria transação

--- bytebank/lib/database/http/webclient.dart
+++ bytebank/lib/database/http/webclient.dart
@@ -1,3 +1,6 @@
+import 'dart:convert';
+import 'package:bytebank/models/contact.dart';
+import 'package:bytebank/models/transaction.dart';
 import 'package:http/http.dart';
 import 'package:bytebank/database/http/webclient.dart';
 import 'package:http_interceptor/http_interceptor.dart';
@@ -23,7 +26,22 @@ class LoggingInterceptor implements InterceptorContract {
 
 }
 
-void findAll() async {
+Future<List<Transaction>> findAll() async {
   final Client client = HttpClientWithInterceptor.build(interceptors: [LoggingInterceptor()]);
   final Response response = await client.get('http://192.168.0.20:8080/transactions');
+  final List<dynamic> decodedJson = jsonDecode(response.body);
+  final List<Transaction> transactions = List();
+  for(Map<String, dynamic> transactionJson in decodedJson) {
+    final Map<String, dynamic> contactJson = transactionJson['contact'];
+    final Transaction transaction = Transaction(
+      transactionJson['value'], 
+      Contact(
+        0,
+        contactJson['name'], 
+        contactJson['contact']['accountNumber'],
+      )
+    );
+    transactions.add(transaction);
+  }
+  return transactions;
 }
--- bytebank/lib/main.dart
+++ bytebank/lib/main.dart
@@ -6,7 +6,7 @@ import 'package:bytebank/database/http/webclient.dart';
 
 void main() {
   runApp(BytebankApp());
-  findAll();
+  findAll().then((transactions) => print('new transactions $transactions'));
 }
 
 class BytebankApp extends StatelessWidget {
