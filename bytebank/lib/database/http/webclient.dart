1fc5dc8f961b94d2619a4c5bc4e42982be989e47
JoyceMassau <joyce@coesma.com.br>
2020-04-15 14:12:48 -0300

    Declara classe do interceptador http

--- bytebank/lib/database/http/webclient.dart
+++ bytebank/lib/database/http/webclient.dart
@@ -1,5 +1,24 @@
 import 'package:http/http.dart';
 import 'package:bytebank/database/http/webclient.dart';
+import 'package:http_interceptor/interceptor_contract.dart';
+import 'package:http_interceptor/models/request_data.dart';
+import 'package:http_interceptor/models/response_data.dart';
+
+class LoggingInterceptor implements InterceptorContract {
+  @override
+  Future<RequestData> interceptRequest({RequestData data}) async {
+    print(data);
+    return data;
+  }
+
+  @override
+  Future<ResponseData> interceptResponse({ResponseData data}) async {
+      print(data);
+      return data;
+  }
+
+}
+
 void findAll() async {
   final Response response = await get('http://192.168.0.20:8080/transactions');
   print(response.body);
