0ec0b4107c145466aba35176998f2583589aee83
JoyceMassau <joyce@coesma.com.br>
2020-04-17 10:42:31 -0300

    Extrai Webclient para Interceptor

--- bytebank/lib/database/http/webclient.dart
+++ bytebank/lib/database/http/webclient.dart
@@ -5,25 +5,7 @@ import 'package:http/http.dart';
 import 'package:bytebank/database/http/webclient.dart';
 import 'package:http_interceptor/http_interceptor.dart';
 
-class LoggingInterceptor implements InterceptorContract {
-  @override
-  Future<RequestData> interceptRequest({RequestData data}) async {
-    print('Request');
-    print('url: ${data.url}');
-    print('headers: ${data.headers}');
-    print('body: ${data.body}');
-    return data;
-  }
-
-  @override
-  Future<ResponseData> interceptResponse({ResponseData data}) async {
-    print('Response');
-    print('status code: ${data.statusCode}');
-    print('headers: ${data.headers}');
-    print('body: ${data.body}');
-    return data;
-  }
-}
+import 'interceptors/logging_interceptor.dart';
 
 const String baseUrl = 'http://192.168.0.20:8080/transactions';
 
