b6739cb656c2766b3a799bd6d8c345df278ccd28
JoyceMassau <joyce@coesma.com.br>
2020-04-17 17:14:13 -0300

    Introduz mensagem de sucesso

--- bytebank/lib/screens/transactions_form.dart
+++ bytebank/lib/screens/transactions_form.dart
@@ -83,7 +83,10 @@ class _TransactionFormState extends State<TransactionForm> {
 void _save(Transaction transactionCreated, String password, BuildContext context) async {
   _webClient.save(transactionCreated, password).then((transaction) {
     if(transaction != null) {
-      Navigator.pop(context);
+      showDialog(context: context, builder: (contextDialog) {
+        return SuccessDialog('Successful transaction');
+      }).then((value) => Navigator.pop(context));
+ 
     }
   }).catchError((e) {
     showDialog(context: context, builder: (contextDialog) {
