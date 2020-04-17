4e1b8440a44a9e953a36cd7bf10bdfaab55a08b3
JoyceMassau <joyce@coesma.com.br>
2020-04-17 10:38:46 -0300

    Enviando transferencia para a web API

--- bytebank/lib/screens/contacts_list.dart
+++ bytebank/lib/screens/contacts_list.dart
@@ -2,6 +2,7 @@ import 'package:bytebank/components/progress.dart';
 import 'package:bytebank/database/dao/contact_dao.dart';
 import 'package:bytebank/models/contact.dart';
 import 'package:bytebank/screens/contact_form.dart';
+import 'package:bytebank/screens/transactions_form.dart';
 import 'package:flutter/material.dart';
 
 class ContactsList extends StatelessWidget {
@@ -31,7 +32,16 @@ class ContactsList extends StatelessWidget {
               return ListView.builder(
                 itemBuilder: (context, index) {
                   final Contact contact = contacts[index];
-                  return _ContactItem(contact);
+                  return _ContactItem(
+                    contact, 
+                    onClick: () {
+                      Navigator.of(context).push(
+                        MaterialPageRoute(
+                          builder: (context) => TransactionForm(contact),
+                        ),
+                      );
+                    }
+                  );
                 },
                 itemCount: contacts.length,
               );
@@ -55,13 +65,18 @@ class ContactsList extends StatelessWidget {
 class _ContactItem extends StatelessWidget {
 
   final Contact contact;
+  final Function onClick;
 
-  _ContactItem(this.contact);
+  _ContactItem(
+    this.contact, {
+    @required this.onClick
+  });
 
   @override
   Widget build(BuildContext context) {
     return Card(
       child: ListTile(
+        onTap: () => onClick(),
         title: Text(contact.name, style: TextStyle(fontSize: 24.0),),
         subtitle: Text(contact.accountNumber.toString(), style: TextStyle(fontSize: 16.0),),
       )
