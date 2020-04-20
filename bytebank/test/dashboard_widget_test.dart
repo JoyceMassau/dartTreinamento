975a382c2f429f3628b715cf267e94a2c20d55c6
JoyceMassau <joyce@coesma.com.br>
2020-04-20 13:45:34 -0300

    Implementa testes de widget

---
+++ bytebank/test/dashboard_widget_test.dart
@@ -0,0 +1,18 @@
+import 'package:bytebank/main.dart';
+import 'package:bytebank/screens/dashboard.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  testWidgets('Verifica se é apresentada na tela imagem principal quando dashboard é aberto', (WidgetTester tester) async {
+    await tester.pumpWidget(MaterialApp(home: Dashboard()));
+    final mainImage = find.byType(Image);
+    expect(mainImage, findsOneWidget);
+  });
+
+  testWidgets('Verifica se é apresentada na tela a primeira funcionalidade quando dashboard é aberto', (tester) async {
+    await tester.pumpWidget(MaterialApp(home: Dashboard()));
+    final firstFeature = find.byType(FeatureItem);
+    expect(firstFeature, findsWidgets);
+  });
+}
