9b0ddfc89ceaaa84017faa3ff7a6ed176eab4132
JoyceMassau <joyce@coesma.com.br>
2020-04-20 13:58:59 -0300

    Corrige visualização do dashboard após rolagem tela

--- bytebank/lib/screens/dashboard.dart
+++ bytebank/lib/screens/dashboard.dart
@@ -10,35 +10,40 @@ class Dashboard extends StatelessWidget {
       appBar: AppBar(
         title: Text('Dashboard'),
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Image.asset('images/bytebank_logo.png'),
-            ),
-            Container(
-              height: 120,
-              child: ListView(
-                scrollDirection: Axis.horizontal,
-                children: <Widget>[
-                  FeatureItem(
-                    'Transfer', 
-                    Icons.monetization_on,
-                    onClick: () =>
-                      _showContactsList(context),
+      body: LayoutBuilder(
+        builder: (context, constraints) => SingleChildScrollView(
+          child: ConstrainedBox(
+            constraints: BoxConstraints(minHeight: constraints.maxHeight),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Image.asset('images/bytebank_logo.png'),
+                ),
+                Container(
+                  height: 120,
+                  child: ListView(
+                    scrollDirection: Axis.horizontal,
+                    children: <Widget>[
+                      FeatureItem(
+                        'Transfer', 
+                        Icons.monetization_on,
+                        onClick: () =>
+                          _showContactsList(context),
+                      ),
+                      FeatureItem(
+                        'Transaction Feed', 
+                        Icons.description,
+                        onClick: () => _showTransactionsList(context),
+                      )
+                    ],
                   ),
-                  FeatureItem(
-                    'Transaction Feed', 
-                    Icons.description,
-                    onClick: () => _showTransactionsList(context),
-                  )
-                ],
-              ),
+                ),
+              ],
             ),
-          ],
+          ),
         ),
       ),
     );
