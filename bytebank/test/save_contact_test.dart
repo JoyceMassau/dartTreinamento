97f0b9a36036d197758b1a2661f7aedd4ec31b14
JoyceMassau <joyce@coesma.com.br>
2020-04-22 08:22:13 -0300

    Verifica que há testes que não dá para usar o Pump

--- bytebank/test/save_contact_test.dart
+++ bytebank/test/save_contact_test.dart
@@ -1,4 +1,5 @@
 import 'package:bytebank/main.dart';
+import 'package:bytebank/screens/contact_form.dart';
 import 'package:bytebank/screens/contacts_list.dart';
 import 'package:bytebank/screens/dashboard.dart';
 import 'package:flutter_test/flutter_test.dart';
@@ -16,10 +17,19 @@ void main() {
     expect(transferFeatureItem, findsOneWidget);
     await tester.tap(transferFeatureItem);
     await tester.pump(); //Rebuild do widget para fazer a próxima microtarefa no meio do caminho até o conteúdo ser carregado
-    await tester.pump(); //Rebuild do widget para fazer a próxima microtarefa no meio do caminho até o conteúdo ser carregado
-    await tester.pump(); //Rebuild do widget para fazer a próxima microtarefa no meio do caminho até o conteúdo ser carregado
+    await tester.pump(); 
+    await tester.pump(); 
     
     final contactsList = find.byType(ContactsList);
     expect(contactsList, findsOneWidget);
+
+    final fabNewContact = find.widgetWithIcon(FloatingActionButton, Icons.add);
+    expect(fabNewContact, findsOneWidget);
+    await tester.tap(fabNewContact);
+
+    final contactForm = find.byType(ContactForm);
+    expect(contactForm, findsOneWidget);
+    //Se tiver outra microtarefa pendente de ser executada, pump não vai funcionar
+  
   });
 }
